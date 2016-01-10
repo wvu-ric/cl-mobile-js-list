b9322762561aee67e8157e364ebd69f355abdf1e
rickykirkendall <rickykirkendall@flocoapps.com>
2016-01-10 13:40:55 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,18 @@
+var input;
+var items = [];
+var count = 0;
+
+while(input != ""){
+	input = prompt("Enter item");
+
+	
+}
+
+
+count = 0;
+var toDisplay = "";
+while(count < items.length){
+	
+	
+}
+alert(toDisplay);
