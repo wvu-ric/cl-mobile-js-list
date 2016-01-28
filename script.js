2764595e340c7b69169399743e805574cff68131
Logan Spears <loganjspears@gmail.com>
2016-01-28 16:18:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 var input;
 var items = [];
-var count = 0;
 
 while(input != ""){
 	input = prompt("Enter item");
-
 	
 }
 
-
-count = 0;
+var count = 0;
 var toDisplay = "";
 while(count < items.length){
-	
-	
+	count++;
 }
 alert(toDisplay);
