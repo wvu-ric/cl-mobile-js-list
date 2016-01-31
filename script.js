7c8f4a7e45ccdbcbaf8aa411cc6e8f193fb9ff23
rwatalin <rwatalin@mix.wvu.edu>
2016-01-31 17:48:54 -0500

    made some additions but haven't been able to remove the commas

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+// create variables
 var input;
 var items = [];
 var count = 0;
 
+// while the input is not blank, prompt user to enter an item
 while(input != ""){
 	input = prompt("Enter item");
-
-	
+// this appends the input into items as a list
+	items.push(input);
 }
 
-
-count = 0;
+// create variables
 var toDisplay = "";
+var numbered;
+var numberedList =[];
+
+// while count is less than the item length,
 while(count < items.length){
-	
-	
+//append number (by adding 1 to the count) and (eg 1. ) to the items and save it as the variable numbered
+	numbered =  count+1 + "." + items[count] + "\n";
+//	numberedNoComma = numbered.replace(/,/g, ''))
+// this appends the numbered items into an array
+ numberedList.push(numbered)
+//increment count by 1
+		count++;
+		alert(numberedList)
 }
-alert(toDisplay);
+
+// remove the last item from the list
+//toDisplay = numberedList.pop()
+
+//alert(toDisplay);
