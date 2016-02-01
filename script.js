4fddf71f062591e894c9894b409248c9ff702b1f
rwatalin <rwatalin@mix.wvu.edu>
2016-01-31 21:21:35 -0500

    Completed List Assignment RW
    Was not able to remove the comma previously because I did not notice
    the extra parenthesis on the code.

--- script.js
+++ script.js
@@ -10,24 +10,27 @@ while(input != ""){
 	items.push(input);
 }
 
+// remove the last item from the array
+items.pop();
+
 // create variables
 var toDisplay = "";
 var numbered;
-var numberedList =[];
+var numberedList = [];
 
 // while count is less than the item length,
 while(count < items.length){
 //append number (by adding 1 to the count) and (eg 1. ) to the items and save it as the variable numbered
 	numbered =  count+1 + "." + items[count] + "\n";
-//	numberedNoComma = numbered.replace(/,/g, ''))
 // this appends the numbered items into an array
- numberedList.push(numbered)
+ numberedList.push(numbered);
 //increment count by 1
 		count++;
-		alert(numberedList)
 }
 
-// remove the last item from the list
-//toDisplay = numberedList.pop()
+//this converts the array to a string
+var numberedString = numberedList.toString();
+// removes any commas in the string
+toDisplay = numberedString.replace(/,/g, '');
 
-//alert(toDisplay);
+alert(toDisplay);
