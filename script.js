af3201079ba071a573ef4ab92659ca2a11c9cbeb
jnelso19 <jnelso19@mix.wvu.edu>
2016-02-02 10:13:19 -0500

    Nelson list

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ var count = 0;
 while(input != ""){
 	input = prompt("Enter item");
 
-	
+
 }
 
 
 count = 0;
 var toDisplay = "";
 while(count < items.length){
-	
-	
+var groceries = ["Apples", "Oranges", "Milk"];
+
+console.log(groceries.length)
+
 }
 alert(toDisplay);
