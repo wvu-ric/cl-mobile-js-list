d07bd9e6fccb30901d9a3e0dd746a99df81d89f9
jcordonier <cordonierjf@gmail.com>
2016-02-02 14:49:32 -0500

    List almost complete
    I couldn't figure out how to append a numerical value to each item in
    the array.

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ var items = [];
 
 while(input != ""){
 	input = prompt("Enter item");
-	
+	items.push(input);
 }
 
 var count = 0;
 var toDisplay = "";
 while(count < items.length){
 	count++;
-}
-alert(toDisplay);
+	}
+		//alert(count + 1 + ". " + items[count]);
+
+alert(items.join('\n'));
+
+
+//items.join('\n')
